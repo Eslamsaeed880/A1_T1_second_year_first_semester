5e449acac0a922a1c2b4c8b4e02b470681cf2077
eslam <eslamsaeed298@gmail.com>
2024-10-01 23:37:38 +0300

    Doing the class and part of the first program

--- A1_T1_Sec_20230060.cpp
+++ A1_T1_Sec_20230060.cpp
@@ -14,9 +14,51 @@ using namespace std;
 // Date 8 oct 2024
 // Detailed explanation of the file and how program works ...
 
+class objectives
+{
+private:
+    string choice;
 
+    void MaleToInclusive()
+    {
+        string sentence;
+        string newsentence;
+        getline(cin, sentence);
+
+        int i = 0, j = 0;
+
+        while(i < sentence.size() && j < sentence.size())
+        {
+            if()
+        }
+    }
+
+public:
+    objectives(){choice = "";};
+    ~objectives(){};
+
+    void hello()
+    {
+        cout << "Hello, This is our sheet, It does some programs and solves some problems\n"
+                "Choose the number of the problem You want\n"
+                "1: The male speech to inclusive speech converter\n"
+                "2: \n"
+                "3: \n"
+                "4: \n";
+
+        do
+        {
+            cout << "Choose a Number from 1 to 4\n";
+            cin >> choice;
+        }
+        while(choice < "1" || choice > "4");
+    }
+
+
+};
 
 int main()
 {
-
+    objectives ob;
+    ob.hello();
 }
