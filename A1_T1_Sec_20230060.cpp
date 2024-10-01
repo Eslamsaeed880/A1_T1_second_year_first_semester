b6592fe317ef03d87a5f6275778b49ef66d61928
eslam <eslamsaeed298@gmail.com>
2024-10-01 22:55:26 +0300

    Creating the header

--- A1_T1_Sec_20230060.cpp
+++ A1_T1_Sec_20230060.cpp
@@ -1,6 +1,22 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
 using namespace std;
+
+// File: A1_T1_Sec_20230060
+// Purpose:
+// Author:
+// Section:
+// ID:
+// TA:
+// Date 8 oct 2024
+// Detailed explanation of the file and how program works ...
+
+
+
 int main()
 {
-    
+
 }
